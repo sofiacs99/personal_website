e0cf1c921ba4aa03d3c85496f36fc2f5130708f5
Sofia Cacho Sousa <sofiacsv@gmail.com>
2022-02-05 14:46:54 -0500

    NEW CSS

--- jquery.js
+++ jquery.js
@@ -38,4 +38,52 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+window.onload = function() {
+  imgs = document.getElementById('slideshow').children;
+  interval = 4000;
+  currentPic = 0;
+  imgs[currentPic].style.webkitAnimation = 'fadey '+interval+'ms';
+  imgs[currentPic].style.animation = 'fadey '+interval+'ms';
+  var infiniteLoop = setInterval(function(){
+    imgs[currentPic].removeAttribute('style');
+    if ( currentPic == imgs.length - 1) { currentPic = 0; } else { currentPic++; }
+    imgs[currentPic].style.webkitAnimation = 'fadey '+interval+'ms';
+    imgs[currentPic].style.animation = 'fadey '+interval+'ms';
+  }, interval);
+}
 
+$(function(){
+  $(".draggable").draggable({
+      stack: ".draggable",
+      containment: ".container"
+  });
+
+  $(".switch").click( function() {
+      $(".container").toggleClass("darkmode");
+  })
+
+  $(".clickme").click( function() {
+      $(".extra-info").fadeToggle("hide");
+  })
+
+  $(".hoverme").hover( function() {
+      $(".gif").fadeToggle("gif");
+  })
+
+  $(".draggable2").draggable({
+      axis: "x"
+  });
+
+  $(".draggable2").draggable({
+      stack: ".draggable",
+      containment: ".container"
+  });
+
+  $(".portrait").click( function() {
+      $(".portrait").animate({
+          width: "600px",
+          height: "600px"
+      })
+  })
+
+})
